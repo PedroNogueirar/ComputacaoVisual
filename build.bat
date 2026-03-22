91ecc4e6110305b100c59a6d9642112d22fb0317
Pedro Nogueira <73959558+PedroNogueirar@users.noreply.github.com>
2026-03-22 18:31:52 -0300

    Add files via upload
    Implementando Feature 1 e 2 do Projeto

---
+++ build.bat
@@ -0,0 +1,1 @@
+gcc -Wall -g "D:\ComputacaoVisual\main.c" -o "D:\ComputacaoVisual\output\main.exe" -I "D:\ComputacaoVisual\lib\SDL3\include" -L "D:\ComputacaoVisual\lib\SDL3\lib" -lSDL3 -lSDL3_image
